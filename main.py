bce91e10b57e293d3e5f73e79283a54c87c320cf
Oskar <saturdaysheroes.business@gmail.com>
2020-09-13 18:57:09 +0200

    generate certificate and assign client

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hi")
+from vulcan import Vulcan
+import config
+import json
+from os import path
+
+# Generate Certificate.
+if path.exists("cert.json"):
+    with open("cert.json") as f:
+        certificate = json.load(f)
+else:
+    certificate = Vulcan.register(config.credentials[0], config.credentials[1], config.credentials[2])
+    with open("cert.json", "w") as f:
+        json.dump(certificate.json, f)
+
+# Initialize Client.
+client = Vulcan(certificate)
+
+
