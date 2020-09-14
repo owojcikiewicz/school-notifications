7388e19b3897fd8dd1a8fe90bb4e196004d2df4d
Oskar <saturdaysheroes.business@gmail.com>
2020-09-14 19:53:11 +0200

    changed structure

--- school-notifications/main.py
+++ school-notifications/main.py
@@ -4,12 +4,12 @@ import json
 from os import path
 
 # Generate Certificate.
-if path.exists("cert.json"):
-    with open("cert.json") as f:
+if path.exists("./cert.json"):
+    with open("./cert.json") as f:
         certificate = json.load(f)
 else:
     certificate = Vulcan.register(config.credentials[0], config.credentials[1], config.credentials[2])
-    with open("cert.json", "w") as f:
+    with open("./cert.json", "w") as f:
         json.dump(certificate.json, f)
 
 # Initialize Client.
