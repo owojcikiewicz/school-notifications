05246175e0046dff86ac9730c3a6b0bee7d87f28
Oskar <saturdaysheroes.business@gmail.com>
2021-03-20 13:35:14 +0100

    updated to new API

--- school-notifications/main.py
+++ school-notifications/main.py
@@ -1,9 +1,9 @@
 import json
 import config
-import schedule
 import time
+import asyncio
 from datetime import date, timedelta, datetime
-from utils import client, message_cache, homework_cache, save_messages, read_message, save_homework
+from utils import setup_client, save_homework
 from discord_webhook import DiscordWebhook
 
 def log(message):
@@ -17,61 +17,46 @@ def send_webhook(content):
     except:
         log("Error while sending webhook occurred!")
 
-
-def send_message(message):
-    text = "<@%s>\n**Tytuł:** %s\n**Nadawca:** %s\n**Godzina:** %s\n**Treść:**```%s```" % (
-        config.discord_user_id, message.title, message.sender.name, message.sent_time.strftime("%H:%M"), message.content
-    )
-    send_webhook(text)
-
 def send_homework(homework):
-    text = "<@%s>\n**Przedmiot:** %s\n**Termin:** %s\n**Treść:**```%s```" % (
-        config.discord_user_id, homework.subject.name, homework.date.strftime("%d.%m.%Y"), homework.description
+    str_date = str(homework.deadline)
+    date_raw = str_date.split()[0].split("-")
+    date_parsed = date_raw[2] + "." + date_raw[1] + "." + date_raw[0]
+
+    text = "@here\n**Przedmiot:** %s\n**Termin:** %s\n**Treść:**```%s```" % (
+        homework.subject.name, date_parsed, homework.content
     )
     send_webhook(text)
 
+async def check_homework(client, homework_cache):
+    log("Checking homework...")
 
-def check_messages():
-    log("Checking messages...")
-    messages_list = list(client.get_messages())
-    for message in messages_list:
-        if not message or not message.sender:
-            message_cache.append(message.id)
-            log("Invalid message, skipping...")
+    homework_raw = await client.data.get_homework(date.today() - timedelta(days=1))
+    async for homework in homework_raw: 
+        if not homework: 
+            homework_cache.append(homework.id)
+            log("Invalid homework, skipping...")
             continue
 
-        if message.id in message_cache:
-            log("Message found in cache, skipping...")
-            break
-        else:
-            message_cache.append(message.id)
-            send_message(message)
-            read_message(message.id)
-            save_messages()
+        if homework.id in homework_cache:
+            log("Homework found in cache, skipping...")
+            continue
+        else: 
+            homework_cache.append(homework.id)  
+            send_homework(homework)
+            save_homework(homework_cache)
 
-def check_homework():
-    log("Checking homework...")
-    for i in range(6): 
-        homework = client.get_homework(date.today() + timedelta(days=i))
-        homework_list = list(homework)
-        for homework in homework_list: 
-            if not homework: 
-                homework_cache.append(homework.id)
-                log("Invalid homework, skipping...")
-                continue
+async def main():
+    global homework_cache 
 
-            if homework.id in homework_cache:
-                log("Homework found in cache, skipping...")
-                continue
-            else: 
-                homework_cache.append(homework.id)
-                send_homework(homework)
-                save_homework()
+    log("Starting app...")
+    client, homework_cache = await setup_client()
 
+    # Check Homework.
+    await check_homework(client, homework_cache)
 
-log("Starting app...")
-schedule.every(config.check_time).minutes.do(check_messages)
-schedule.every(config.check_time).minutes.do(check_homework)
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+    log("Stopping app...")
+    await client.close()
+
+if __name__ == "__main__": 
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
--- school-notifications/utils.py
+++ school-notifications/utils.py
@@ -1,47 +1,38 @@
 import config
 import json
 from os import path
-from vulcan import Vulcan
+from vulcan import Keystore, Account, VulcanHebe, Vulcan
 
-if path.exists("./cert.json"):
-    with open("./cert.json") as f:
-        certificate = json.load(f)
-        client = Vulcan(certificate)
-else:
-    certificate = Vulcan.register(config.credentials[0], config.credentials[1], config.credentials[2])
-    client = Vulcan(certificate)
-    with open("./cert.json", "w") as f:
-        json.dump(certificate.json, f)
+async def setup_client():
+    if path.exists("./keystore.json"):
+        with open("keystore.json") as f:
+            keystore = Keystore.load(json.load(f))
+    else: 
+        with open("./keystore.json", "w") as f:
+            keystore = Keystore.create(device_model="Vulcan API")
+            json.dump(keystore.as_dict, f)
 
-if path.exists("./messages.txt"):
-    with open("./messages.txt") as f:
-        message_cache = json.load(f)
-else:
-    message_cache = []
-    with open("./messages.txt", "w") as f:
-        json.dump(message_cache, f)
+    if path.exists("./account.json"):
+        with open("./account.json") as f:
+            account = Account.load(json.load(f))
+    else:
+        account = await Account.register(keystore, config.credentials[0], config.credentials[1], config.credentials[2])
+        with open("./account.json", "w") as f:
+            json.dump(account.as_dict, f)
 
-if path.exists("./homework.txt"):
-    with open("./homework.txt") as f:
-        homework_cache = json.load(f)
-else:
-    homework_cache = []
-    with open("./homework.txt", "w") as f:
-        json.dump(homework_cache, f)
+    if path.exists("./homework.txt"):
+        with open("./homework.txt") as f:
+            homework_cache = json.load(f)
+    else:
+        homework_cache = []
+        with open("./homework.txt", "w") as f:
+            json.dump(homework_cache, f)
 
-def save_homework(): 
+    client = VulcanHebe(keystore, account)
+    await client.select_student()
+
+    return client, homework_cache
+
+def save_homework(homework_cache): 
     with open("./homework.txt", "w") as fl: 
-        json.dump(homework_cache, fl) 
-
-def save_messages():
-    with open("./messages.txt", "w") as fl:
-        json.dump(message_cache, fl)
-
-def read_message(message_id):
-    data = {
-        "WiadomoscID": message_id,
-        "FolderWiadomosci": "Usuniete",
-        "Status": "Widoczna"
-    }
-
-    client._api.post("Uczen/ZmienStatusWiadomosci", json=data)
+        json.dump(homework_cache, fl)
