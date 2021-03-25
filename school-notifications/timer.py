128af202ac1814050812fd834af66f0a18e7ca0a
Oskar <saturdaysheroes.business@gmail.com>
2021-03-25 11:51:37 +0100

    added timer support

---
+++ school-notifications/timer.py
@@ -0,0 +1,14 @@
+import sched, time
+import asyncio
+from main import main
+import config
+import os   
+
+s = sched.scheduler(time.time, time.sleep)
+def check(sc): 
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    s.enter(config.check_time, 1, check, (sc,))
+
+s.enter(config.check_time, 1, check, (s,))
+s.run()
