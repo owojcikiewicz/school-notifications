1a8b210e59e9c5c25b4affa4ad9b89aa8c604b0d
Oskar <saturdaysheroes.business@gmail.com>
2021-03-25 11:53:43 +0100

    Update config.py

--- school-notifications/config.py
+++ school-notifications/config.py
@@ -4,8 +4,5 @@ credentials = ["TOKEN", "SYMBOL", "PIN"]
 # Discord webhook URL.
 webhook_url = "https://discordapp.com/api/webhooks/EXAMPLE_URL"
 
-# Discord webhook URL.
-webhook_url = "https://discord.com/api/webhooks/806072340270022696/t4XubZU1ISChtMhp-BT6pb_L7bOZ8-MWxlyaroL56QviK_pSphW7STIMEFIgQDcQLpzd"
-
 # Check time. 
 check_time = 600000
